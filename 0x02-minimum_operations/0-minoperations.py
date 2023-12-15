bdf618f03602560f7b4884288c4ebe13dc078e36
Christine-design-web <wakuthiichristine5gmail.com>
2023-12-15 05:26:38 +0300

    Productive night

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+calculates the fewest number of operarions needed
+to result exactly n H characters in the file
+
+"""
+from sys import setrecursionlimit
+
+
+def minOperations(n):
+
+    setrecursionlimit(10**6)
+
+    count = 0
+    inc = 2  # 3 4 5 6 etc.
+
+    # factor = 2
+    # no even division (prime) inc = n
+
+    if type(n) is not int or n < 2:
+        return 0
+
+    while n != 1:
+        if n % inc == 0:
+            count += inc  # 1
+            n /= inc
+            inc = 1  # resets for next loop
+        inc = inc + 1
+
+    return count
